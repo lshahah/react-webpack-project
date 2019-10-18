635577963c3bec4b812ced403e6683a0b0d84277
刘尚 <liushangb@yonyou.com>
2019-10-18 11:41:42 +0800

    安装react-route和react-redux

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './app.scss';
 
+
 function App() {
     return (
         <div className={style.content}>
-            初始化项目
+            222
         </div>
     );
 }
